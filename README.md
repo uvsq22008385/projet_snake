6644da44593846cda4cbf4d02de4577b25c9518e
Salah-Eddine HARNOUFI <salah-eddine.harnoufi@ensq.uvsq.fr>
2021-04-01 22:41:29 +0200

    base de code du projet snake

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # projet_snake
 programmation du jeu snake
+
+
+**participants:
+HARNOUFI Salah-Eddine (chef projet/syntaxe)
+ENEKEYE BOAYEKAHO Karim Maurel
+ISSOUF Haled
+TANG Leon
+CHERIF Nour
+SISSOKO Abdullah
+**chef de projet:
+HARNOUFI SALAH EDDINE
+
+**chef de syntaxe (pour l'instant):
+HARNOUFI SALAH EDDINE
+
+**lien discord:
+
---
+++ snake.py
@@ -0,0 +1,31 @@
+###############################
+# nom du projet: snake
+# groupe de td: 5
+# participants:
+#   HARNOUFI Salah-Eddine (chef projet/syntaxe)
+#   ENEKEYE BOAYEKAHO Karim Maurel
+#   ISSOUF Haled
+#   TANG Leon
+#   CHERIF Nour
+#   SISSOKO Abdullah
+###############################
+
+######################
+# modules importés: tkinter
+import tkinter as tk
+
+#####################
+# Constantes
+
+#####################
+# Variables globales
+
+#####################
+# Fonctions principales
+
+####################
+# Programme principale
+
+racine = tk.Tk()
+
+racine.mainloop()
