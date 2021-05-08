8cec5c02a13252eb080d3b52a7e3cd21045aea8b
Salah-Eddine HARNOUFI <salah-eddine.harnoufi@ensq.uvsq.fr>
2021-05-08 11:56:44 +0200

    le snake fonctionne desormais graphiquement, pas d'interruption clair lors d'un gameover

--- snake.py
+++ snake.py
@@ -47,7 +47,7 @@ score = 0
 def mouvement_automatique():
     global direction
     move(direction)
-    racine.after(1000, lambda: mouvement_automatique())
+    racine.after(3000, lambda: mouvement_automatique())
 
 
 def affichage_score():
@@ -55,7 +55,11 @@ def affichage_score():
 
     score += 1
 
-    print("score:", score)
+    afficher = "score:" + str(score)
+
+    print(afficher)
+
+    label.configure(text=afficher)
 
 
 def creation_fruit(nzone, mzone):
@@ -83,7 +87,7 @@ def game_loose():
 
 def move(fleche):
     '''deplace le serpent sur la tableau'''
-    global direction, serpent, tableau, nmur, mmur, fruit
+    global direction, serpent, tableau, dessin, nmur, mmur, fruit
 
     if [direction[0] + fleche[0], direction[1] + fleche[1]] == [0, 0]:
         pass
@@ -108,6 +112,8 @@ def move(fleche):
             fruit = creation_fruit(nmur-1, mmur-1)
             tableau[fruit[0]][fruit[1]] = 5
 
+            canvas.itemconfigure(dessin[fruit[0]][fruit[1]], fill="yellow")
+
         else:
             a = serpent[-1][0]
             b = serpent[-1][1]
@@ -117,12 +123,16 @@ def move(fleche):
             # l'anciene position de la queue du serpent redevient du terrain
             tableau[a][b] = 1
 
+            canvas.itemconfigure(dessin[a][b], fill="pink")
+
             serpent[0] = copy.deepcopy([A, B])
 
         if tableau[A][B] == 0:
             game_loose()
         else:
             tableau[A][B] = 3
+
+            canvas.itemconfigure(dessin[A][B], fill="red")
 
     afficher_tableau(tableau)
 
@@ -137,9 +147,11 @@ def quadrillage(n, m):
     mmur = m - 1
 
     tableau = []
+    dessin = []
 
     for i in range(n):
         tableau.append([])
+        dessin.append([])
         for a in range(m):
             k = 1
 
@@ -156,7 +168,26 @@ def quadrillage(n, m):
 
             tableau[i].append(k)
 
-    return tableau
+            taille_case_x = 500/n
+            taille_case_y = 500/m
+
+            x0 = a * taille_case_x
+            y0 = i * taille_case_y
+            x1 = (a + 1) * taille_case_x
+            y1 = (i + 1) * taille_case_y
+
+            if k == 1:
+                color = "pink"
+            elif k == 3:
+                color = "red"
+            elif k == 5:
+                color = "yellow"
+            elif k == 0:
+                color = "blue"
+
+            dessin[i].append(canvas.create_rectangle((x0, y0), (x1, y1), fill=color))
+
+    return tableau, dessin
 
 
 def afficher_tableau(tableau):
@@ -169,12 +200,21 @@ def afficher_tableau(tableau):
 
 ####################
 # Programme principale
-tableau = quadrillage(10, 10)
+
+racine = tk.Tk()
+
+canvas = tk.Canvas(racine, width=500, height=500, bg="white")
+canvas.grid()
+
+label = tk.Label(racine, text="score:0")
+label.grid()
+
+
+tableau, dessin = quadrillage(10, 10)
 
 
 afficher_tableau(tableau)
 
-racine = tk.Tk()
 
 mouvement_automatique()
 
